d49bb1532a974c6d42f22c0dd5f391da14142312
Prashant <hadaprashant2002@gmail.com>
2025-01-24 22:53:57 +0530

    added config file

---
+++ src/config/gemini.ts
@@ -0,0 +1,35 @@
+const apiUrl = import.meta.env.API_KEY;
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+} from "@google/generative-ai";
+  
+  const apiKey = apiUrl;
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash-exp",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function runGemini(prompt: string) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+  }
+  
+  export default runGemini;
