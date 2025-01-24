28624a6ab8ea8d051edb182cd38f2372b988b2aa
Prashant <hadaprashant2002@gmail.com>
2025-01-24 23:38:42 +0530

    set up the context

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ContextProvider from './context/AiContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ContextProvider>
     <App />
-  </StrictMode>,
+  </ContextProvider>,
 )
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly API_KEY: string;
+    readonly VITE_API_KEY: string;
   }
   
   interface ImportMeta {
